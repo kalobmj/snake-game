11e15e35277ddbc373859138c2e19aa814dba7c5
Kalob Murphy-Jones <kalobmurphyjones@gmail.com>
2025-04-13 03:27:07 -0400

    main

---
+++ javascript/script.js
@@ -0,0 +1,43 @@
+// 15 rows, 17 cols
+const grid = document.getElementById('grid');
+
+for (let i=0; i<10; i++) {
+    grid.innerHTML += `
+        <div class='light'></div>
+        <div class='dark'></div>
+        <div class='light'></div>
+        <div class='dark'></div>
+        <div class='light'></div>
+        <div class='dark'></div>
+        <div class='light'></div>
+        <div class='dark'></div>
+        <div class='light'></div>
+        <div class='dark'></div>
+        <div class='light'></div>
+        <div class='dark'></div>
+        <div class='light'></div>
+        <div class='dark'></div>
+        <div class='light'></div>
+        <div class='dark'></div>
+        <div class='light'></div>
+
+        <div class='dark'></div>
+        <div class='light'></div>
+        <div class='dark'></div>
+        <div class='light'></div>
+        <div class='dark'></div>
+        <div class='light'></div>
+        <div class='dark'></div>
+        <div class='light'></div>
+        <div class='dark'></div>
+        <div class='light'></div>
+        <div class='dark'></div>
+        <div class='light'></div>
+        <div class='dark'></div>
+        <div class='light'></div>
+        <div class='dark'></div>
+        <div class='light'></div>
+        <div class='dark'></div>
+
+    `
+}
