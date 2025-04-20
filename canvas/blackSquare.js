8b03455d52f995beed2128b24f89404b03080161
Kalob Murphy-Jones <kalobmurphyjones@gmail.com>
2025-04-19 22:54:39 -0400

    main

--- canvas/blackSquare.js
+++ canvas/blackSquare.js
@@ -1,5 +1,27 @@
-function draw() {
-  // define canvas
+// function draw() {
+//   // define canvas
+//   const context = document.getElementById('canvas').getContext('2d');
+
+//   // create gray canvas
+//   context.fillStyle = 'gray';
+//   context.fillRect(0, 0, 350, 350);
+
+//   // moves black rect starting point
+//   context.translate(100, 50);
+
+//   // loop to create black square
+//   for (let i=0; i<4; i++) {
+//     for (let j=0; j<4; j++) {
+//       context.strokeStyle = 'white'
+//       context.strokeRect(j*50, i*50, 50, 50);
+//       context.fillStyle = 'black';
+//       context.fillRect(j*50, i*50, 50, 50);
+//     }
+//   }
+
+// };
+
+function drawGrid(maxCellsX, maxCellsY, cellSize) {
   const context = document.getElementById('canvas').getContext('2d');
 
   // create gray canvas
@@ -7,33 +29,22 @@ function draw() {
   context.fillRect(0, 0, 350, 350);
 
   // moves black rect starting point
-  context.translate(100, 50);
+  // context.translate(100, 50);
 
-  // loop to create black square
-  for (let i=0; i<4; i++) {
-    for (let j=0; j<4; j++) {
-      context.strokeStyle = 'white'
-      context.strokeRect(j*50, i*50, 50, 50);
-      context.fillStyle = 'black';
-      context.fillRect(j*50, i*50, 50, 50);
-    }
-  }
-
-};
-
-function drawGrid(maxCellsX, maxCellsY, cellSize) {
-
-  const context = document.getElementById('canvas').getContext('2d');
 
   for (let i=0; i<maxCellsX; i++) {
-    for (let j=0; j<maxCellsY; i++) {
-      context.strokeStyle = 'white';
-      context.strokeRect()
+    for (let j=0; j<maxCellsY; j++) {
+      context.strokeStyle = 'gold';
+      context.strokeRect(maxCellsX*cellSize, maxCellsY*cellSize, cellSize, cellSize);
+      context.fillStyle = 'aquamarine';
+      context.fillRect(maxCellsX*cellSize, maxCellsY*cellSize, cellSize, cellSize)
     }
   }
 
 };
+
+drawGrid(4, 4, 50);
 
 // Make a function that draws a grid. The function could take 3 parameters: maxCellsX, maxCellsY, cellSize. In short, maxCellsX and maxCellsY are the number of horizontal and vertical cells and cellSize is the size of each cell. You can use this function in your initial setup to determine the width and height of your canvas (maxCells * cellSize).
 
-draw();
+// draw();
