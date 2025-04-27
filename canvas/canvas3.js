6f07f80947013935b937ec2e88fa7ea184c6a008
Kalob Murphy-Jones <kalobmurphyjones@gmail.com>
2025-04-27 03:47:02 -0400

    comments

--- canvas/canvas3.js
+++ canvas/canvas3.js
@@ -1,9 +1,21 @@
-// define canvas
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
+const canvas = document.getElementById('canvas'); // canvas
+const context = canvas.getContext('2d'); // canvas context
+const cellSize = 30; // play with the size of this
+const fps = 80; // try 70 60 100 etc...
+const rows = 15; // 15 square long
+const cols = 17; // 17 squares wide
+const color1 = '#a2d149'; // light green
+const color2 = '#8bb042'; // dark green
+const snakeLength = 2; // default snake length
+const snakeColor = '#3498db'; // blue
+const appleColor = '#ff3636'; // red
+let direction = ''; // 'right', 'left' etc...
+let directionQueue = ''; // next move
+let score = 0;
 
-const cellSize = 30;
-const fps = 70;
+// canvas dimensions
+canvas.height = rows * cellSize;
+canvas.width = cols * cellSize;
 
 // nested for loops to create game board
 for (let i=0; i<rows; i++) {
@@ -15,25 +27,12 @@ for (let i=0; i<rows; i++) {
     }
 };
 
-// 15 x 17 game board
-const rows = 15;
-const cols = 17;
-
-// set canvas dimensions
-canvas.height = rows * cellSize;
-canvas.width = cols * cellSize;
-
-const color1 = '#a2d149'; // light green
-const color2 = '#8bb042'; // dark green
-
-const snakeLength = 2;
-const snakeColor = '#3498db'; // blue
-
-const appleColor = '#ff3636'; // red
-
-let direction = '';
-let directionQueue = '';
-let score = 0;
 
 // fillRect(x, y, width, height)
 
+// function to draw square
+function createSquare(x, y, color) {
+
+
+
+};
