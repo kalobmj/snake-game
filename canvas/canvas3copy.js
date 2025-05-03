18967f782fa665054dcdaafd00ea6e5ed756e25c
Kalob Murphy-Jones <kalobmurphyjones@gmail.com>
2025-05-03 01:38:31 -0400

    cleanup

--- canvas/canvas3copy.js
+++ canvas/canvas3copy.js
@@ -16,6 +16,7 @@ let directionQueue = ''; // next move
 let appleLocation = []; // coordinates of apple
 let snakeLocation = []; // coordinates of each cell of snake
 let score = 0;
+let gameInterval; // setInterval to use for later
 
 // this will focus user on the canvas
 canvas.setAttribute('tabindex', 1);
@@ -44,7 +45,7 @@ if (storedHighScore) {
     highScore.innerText = `high score: ${storedHighScore}`;
 };
 
-// nested for loops to create game board
+// function takes nested for loops to create game board
 function makeBoard() {
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < cols; j++) {
@@ -56,7 +57,7 @@ function makeBoard() {
     };
 };
 
-makeBoard();
+// makeBoard();
 
 // function to draw square
 function createSquare(x, y, color) {
@@ -84,7 +85,7 @@ function checkOutOfBounds(x, y) {
 };
 
 // function to place snake on board
-function makeSnake2() {
+function makeSnake() {
     let x1 = 4 * cellSize;
     let y1 = 7 * cellSize;
 
@@ -94,14 +95,11 @@ function makeSnake2() {
     snakeLocation.push({ x: x1, y: y1 }); // tail
 };
 
-makeSnake2();
+// makeSnake();
 
 // function to move the snake, it's already placed down at this point, 'right' is the default direction
 function moveSnake() {
-    console.log('snake moving');
     console.log(`snake moving in ${direction} direction`);
-
-    console.log({ snakeLocation })
 
     // coords of snakeHead
     let x1 = snakeLocation[0].x;
@@ -123,23 +121,17 @@ function moveSnake() {
     };
     checkGameStatus();
 
-    // since 
-
-    // get current tail and save it, so we can use its coords to color the board color
+    // copy of tail for later
     let oldTail = { ...snakeLocation[snakeLocation.length - 1] };
 
-    // tail is last element of snakelocation, using pop removes it
+    // tail we will work with
     let tail = snakeLocation.pop();
 
     // update tail with new coords for head (new head)
     tail.x = x1;
     tail.y = y1;
 
-    // at this point we have an updated tail (the new head), and we have a copy of the old tail.
-    // we will do a check to see if our tail (new head) collides with our current fruit coordinates
-
     // checking for body collisions
-
     for (let i = 0; i < snakeLocation.length; i++) {
         if (checkForCollisions(snakeLocation[i].x, snakeLocation[i].y, tail.x, tail.y)) {
             gameOver = true;
@@ -147,26 +139,18 @@ function moveSnake() {
         }
     };
 
-    // if new head takes place of fruit, we will continue to add it to the front, but keep our old tail (this is how the snake grows)
-    // if not, we will just keep the code as it is already
-
+    // if else statement for if snakeHead hits an apple
     if (checkForCollisions(tail.x, tail.y, appleLocation[0].x, appleLocation[0].y)) {
-        // if new head is on a fruit spot, add the tail to the front consuming the fruit
         snakeLocation.unshift(tail);
-        // push the old tail so that the snake grows by one cell
         snakeLocation.push(oldTail);
         score++;
         userScore.innerText = `score: ${score}`;
         makeApple();
     } else {
-        // if new head is not on a fruit spot, just add the tail to the front
         snakeLocation.unshift(tail);
     };
 
-    // // take tail (new head) and add it to the beginning of snakeLocation array
-    // snakeLocation.unshift(tail);
-
-    // coloring in new snake && checking for body collisions
+    // coloring in new snake
     for (let i = 0; i < snakeLocation.length; i++) {
         createSquare(snakeLocation[i].x, snakeLocation[i].y, snakeColor)
     };
@@ -175,8 +159,6 @@ function moveSnake() {
     const isEven = ((oldTail.x + oldTail.y) / cellSize) % 2 === 0;
     const color = isEven ? color1 : color2;
     createSquare(oldTail.x, oldTail.y, color);
-
-
 
     console.log('moveSnake ended');
 };
@@ -204,9 +186,9 @@ function makeApple() {
     createSquare(x1, y1, appleColor);
 };
 
-makeApple();
+// makeApple();
 
-// board addEventListener ArrowLeft, ArrowUp, ArrowDown, ArrowRight
+// board addEventListener for when user pressed arrow keys
 canvas.addEventListener('keydown', (e) => {
     if (e.key === 'ArrowRight' && direction != 'left') {
         console.log('right arrow key pressed');
@@ -223,58 +205,56 @@ canvas.addEventListener('keydown', (e) => {
     };
 });
 
-// possibly wrap all game running functions in one func, and ending game functions in one func
-
-// add more to fps to slow it down
-
-// need to work on snake colliding with it's own body
-
-// when user clicks play button, get snake going using snakeHead location (snakeLocation[0]) to test
+// play button addEventListener
 playButton.addEventListener('click', () => {
-
-    // coords of our snakeHead
-    console.log(snakeLocation[0].x);
-    console.log(snakeLocation[0].y);
-
-    console.log({ appleLocation })
-
-    console.log(appleLocation[0].x);
-    console.log(appleLocation[0].y);
-
     canvas.focus();
 
-    let gameRunningInterval = setInterval(moveSnake, fps)
+    // interval here
+    gameInterval = setInterval(runGame, fps)
     // setInterval(checkGameStatus, 1000)
-
 });
 
-// seperate all function into 3 big functions like:
-// setupGame()
-// startGame()
-// endGame()
+// get game ready
+function setupGame() {
+    gameOver = false;
+    appleLocation = [];
+    snakeLocation = [];
+    direction = 'right'
+    makeBoard();
+    makeSnake();
+    makeApple();
+    score = 0;
+};
 
-// checks if game is over or not
+// start game
+function runGame() {
+    moveSnake();
+    checkGameStatus();
+};
+
+// end game 
+function endGame() {
+
+    // clear interval here
+    clearInterval(gameInterval);
+    score = 0;
+    setupGame();
+
+};
+
+// checks if game is over or not 😏
 function checkGameStatus() {
     if (gameOver) {
-
-        // in future we will not be using window reload, will have a game over screen and button for user to press to restart game...
-
         if (score > storedHighScore) {
             highScore.innerText = `high score: ${score}`
             localStorage.setItem('high-score', score)
         };
-
-        makeBoard();
-        makeSnake2();
-        makeApple();
-        score = 0;
-
-        clearInterval(gameRunningInterval);
-
+        endGame();
         // window.location.reload();
-
     } else {
         // logic for when gameOver === false (game is still going)
         // setInterval(mainWrapperFunction, fps)
     };
 };
+
+setupGame();
